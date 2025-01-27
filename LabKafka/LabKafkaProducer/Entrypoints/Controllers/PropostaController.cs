94afff903521835e8ae252e413e640de1e5a5885
engDaniloOS <36059157+engDaniloOS@users.noreply.github.com>
2025-01-27 20:27:14 -0300

    Add logs de latencia no producer

--- LabKafka/LabKafkaProducer/Entrypoints/Controllers/PropostaController.cs
+++ LabKafka/LabKafkaProducer/Entrypoints/Controllers/PropostaController.cs
@@ -1,6 +1,7 @@
 ﻿using LabKafkaProducer.Domain.Dtos;
 using LabKafkaProducer.Domain.UseCases;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
 
 namespace LabKafkaProducer.Entrypoints.Controllers
 {
@@ -14,7 +15,13 @@ namespace LabKafkaProducer.Entrypoints.Controllers
         [HttpPost]
         public async Task<IActionResult> PublicarProposta([FromBody]PropostaDto propostaDto)
         {
+            var watch = Stopwatch.StartNew();
+
             var propostaPublicada = await _publicarUseCase.Executar(propostaDto);
+
+            watch.Stop();
+
+            Console.WriteLine($"Requisicao processada em {watch.Elapsed.TotalMilliseconds}ms");
 
             if(propostaPublicada.HasError)
                 return BadRequest(propostaPublicada.ErrorMessage);
